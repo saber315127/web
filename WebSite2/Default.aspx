89233dd142d92c8f3e4c1ed977301bfcc4f25c9d
saber315127 <37133549+saber315127@users.noreply.github.com>
2018-12-24 19:09:41 +0800

    Add files via upload

---
+++ WebSite2/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server" Width="65px">1</asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server" Width="77px">2</asp:TextBox>
+        <asp:TextBox ID="TextBox3" runat="server" Width="68px">3</asp:TextBox>
+    
+    </div>
+        <asp:TextBox ID="TextBox4" runat="server" Width="66px">4</asp:TextBox>
+        <asp:TextBox ID="TextBox5" runat="server" Width="76px">5</asp:TextBox>
+        <asp:TextBox ID="TextBox6" runat="server" Width="71px">6</asp:TextBox>
+        <br />
+        <asp:TextBox ID="TextBox7" runat="server" Width="66px">7</asp:TextBox>
+        <asp:TextBox ID="TextBox8" runat="server" Width="76px">8</asp:TextBox>
+        <asp:TextBox ID="TextBox9" runat="server" Width="72px">9</asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox10" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Height="41px" OnClick="Button1_Click" Text="1" Width="45px" />
+        <asp:Button ID="Button2" runat="server" Height="41px" OnClick="Button2_Click" Text="2" Width="41px" />
+        <asp:Button ID="Button3" runat="server" Height="39px" OnClick="Button3_Click" Text="3" Width="45px" />
+        <br />
+        <asp:Button ID="Button4" runat="server" Height="40px" OnClick="Button4_Click" Text="4" Width="44px" />
+        <asp:Button ID="Button5" runat="server" Height="37px" OnClick="Button5_Click" Text="5" Width="42px" />
+        <asp:Button ID="Button6" runat="server" Height="42px" OnClick="Button6_Click" Text="6" Width="46px" />
+        <br />
+        <asp:Button ID="Button7" runat="server" Height="42px" OnClick="Button7_Click" Text="7" Width="44px" />
+        <asp:Button ID="Button8" runat="server" Height="37px" OnClick="Button8_Click" style="margin-top: 0px" Text="8" Width="44px" />
+        <asp:Button ID="Button9" runat="server" Height="40px" OnClick="Button9_Click" Text="9" Width="44px" />
+    </form>
+</body>
+</html>
---
+++ WebSite2/Default.aspx.cs
@@ -0,0 +1,627 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public class Global
+
+    {
+        public static int a = 1;
+
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (Global.a %2==1)
+        {
+            TextBox10.Text = "";
+            TextBox1.Text = "X";
+                if ((TextBox2.Text == TextBox1.Text) && (TextBox3.Text == TextBox1.Text))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox4.Text == "X") && (TextBox7.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "X") && (TextBox9.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+            
+            
+        }
+        else
+        {
+            TextBox10.Text = "";
+            TextBox1.Text = "O";
+                if ((TextBox2.Text == "O") && (TextBox3.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox4.Text == "O") && (TextBox7.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "O") && (TextBox9.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+           
+            
+        }
+        if (Global.a>=9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        if (Global.a % 2 == 1)
+        {
+            TextBox10.Text = "";
+            TextBox2.Text = "X";
+                if ((TextBox1.Text == "X") && (TextBox3.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "X") && (TextBox6.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+           
+            
+        }
+        else
+        {
+            TextBox10.Text = "";
+            TextBox2.Text = "O";
+                if ((TextBox1.Text == "O") && (TextBox3.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "O") && (TextBox6.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+            
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        if (Global.a % 2 == 1)
+        {
+            TextBox10.Text = "";
+            TextBox3.Text = "X";
+                if ((TextBox1.Text == "X") && (TextBox2.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox6.Text == "X") && (TextBox9.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "X") && (TextBox7.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+
+            
+            
+        }
+        else
+        {
+            TextBox10.Text = "";
+            TextBox3.Text = "O";
+                if ((TextBox6.Text == "O") && (TextBox9.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox1.Text == "O") && (TextBox2.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "O") && (TextBox7.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                
+            
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        
+            if (TextBox4.Text == "")
+            {
+                TextBox4.Text = "X";
+                if ((TextBox1.Text == "X") && (TextBox7.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "X") && (TextBox6.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+            
+            
+        }
+        else
+        {
+            TextBox10.Text = "";
+            TextBox4.Text = "O";
+                if ((TextBox1.Text == "O") && (TextBox7.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "O") && (TextBox6.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+           
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        if (Global.a % 2 == 1)
+        {
+            TextBox10.Text = "";
+            TextBox5.Text = "X";
+                if ((TextBox6.Text == "X") && (TextBox4.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox8.Text == "X") && (TextBox2.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox3.Text == "X") && (TextBox7.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox1.Text == "X") && (TextBox9.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+            
+            
+        }
+        else
+        {
+            TextBox10.Text = "";
+            TextBox5.Text = "O";
+                if ((TextBox6.Text == "O") && (TextBox4.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox8.Text == "O") && (TextBox2.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox3.Text == "O") && (TextBox7.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox1.Text == "O") && (TextBox9.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+            
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        if (Global.a % 2 == 1)
+        {
+            
+                TextBox6.Text = "X";
+                if ((TextBox3.Text == "X") && (TextBox9.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "X") && (TextBox4.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+            
+            
+        }
+        else
+        {
+            
+                TextBox6.Text = "O";
+                if ((TextBox3.Text == "O") && (TextBox9.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "O") && (TextBox4.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+           
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        if (Global.a % 2 == 1)
+        {
+            TextBox10.Text = "";
+            TextBox7.Text = "X";
+                if ((TextBox1.Text == "X") && (TextBox4.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "X") && (TextBox3.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox8.Text == "X") && (TextBox9.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+           
+            
+        }
+        else
+        {
+            
+                TextBox7.Text = "O";
+                if ((TextBox1.Text == "O") && (TextBox4.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox5.Text == "O") && (TextBox3.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox8.Text == "O") && (TextBox9.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+           
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        if (Global.a % 2 == 1)
+        {
+            TextBox10.Text = "";
+            TextBox8.Text = "X";
+                if ((TextBox2.Text == "X") && (TextBox5.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox7.Text == "X") && (TextBox9.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+           
+            
+        }
+        else
+        {
+            
+                TextBox8.Text = "O";
+                if ((TextBox2.Text == "O") && (TextBox5.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox7.Text == "O") && (TextBox9.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+            
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        if (Global.a % 2 == 1)
+        {
+            TextBox10.Text = "";
+            if (TextBox9.Text != "X" || TextBox9.Text != "O")
+            {
+                TextBox9.Text = "X";
+                if ((TextBox3.Text == "X") && (TextBox6.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox7.Text == "X") && (TextBox8.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox1.Text == "X") && (TextBox5.Text == "X"))
+                {
+                    TextBox10.Text = "X is winner";
+                    Global.a = 10;
+                }
+            }
+            else
+            {
+                TextBox10.Text = "已經被選了";
+            }
+            
+            
+        }
+        else
+        {
+            if (TextBox9.Text != "X" || TextBox9.Text != "O")
+            {
+                TextBox10.Text = "";
+                TextBox9.Text = "O";
+                if ((TextBox3.Text == "O") && (TextBox6.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox7.Text == "O") && (TextBox8.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+                if ((TextBox1.Text == "O") && (TextBox5.Text == "O"))
+                {
+                    TextBox10.Text = "O is winner";
+                    Global.a = 10;
+                }
+            }
+            else
+            {
+                TextBox10.Text = "已經被選了";
+                Global.a -= 1;
+            }
+            
+        }
+        if (Global.a >= 9)
+        {
+            TextBox1.Text = "1";
+            TextBox2.Text = "2";
+            TextBox3.Text = "3";
+            TextBox4.Text = "4";
+            TextBox5.Text = "5";
+            TextBox6.Text = "6";
+            TextBox7.Text = "7";
+            TextBox8.Text = "8";
+            TextBox9.Text = "9";
+            Global.a = 1;
+            if (Global.a == 9)
+            {
+                TextBox10.Text = "遊戲結束";
+            }
+        }
+        else
+        {
+            Global.a += 1;
+        }
+    }
+}
